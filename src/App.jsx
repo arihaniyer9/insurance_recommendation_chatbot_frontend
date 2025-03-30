ce5e718a3d04135c8ee5419529ea038f833e3796
ShubhJani14 <114872718+ShubhJani14@users.noreply.github.com>
2025-03-29 23:02:58 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,8 @@ import SignupPage from './pages/SignupPage';
 import Dashboard from './pages/Dashboard';
 import ChatInterface from './pages/ChatInterface';
 import InsuranceInfoPage from './pages/InsuranceInfoPage';
-import CommunityPage from './pages/CommunityPage';
 import UserAccountPage from './pages/UserAccountPage';
-
+import CrowdFunding from './pages/CrowdFunding';
 function App() {
   return (
     <Router>
@@ -18,13 +17,12 @@ function App() {
         <Route path="/signup" element={<SignupPage/>} />
         <Route path="/dashboard" element={<Dashboard/>} />
         <Route path="/chat" element={<ChatInterface/>} />
-        <Route path="/community" element={<CommunityPage/>} />
         <Route path="/info" element={<InsuranceInfoPage/>} />
         <Route path="/acct" element={<UserAccountPage/>} />
+        <Route path="/crowd" element={<CrowdFunding/>} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
